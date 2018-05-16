6db906da8e5acb3745c3561fd7b36a005aa8b431
Meidor <rikharink@gmail.com>
2018-05-16 22:53:21 +0200

    Clean slate

--- scripts/popup.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var app = angular.module('popup', []);
-app.controller("PopupController", function($scope){
-	$scope.openOptions = function(){
-		chrome.tabs.create({'url': chrome.extension.getURL('options.html')}, function(tab) {
-		});
-	}
-})
